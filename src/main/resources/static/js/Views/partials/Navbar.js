b68d741ef2c1c045a851e9f2ba2f699ac476c8a9
shawcorey <shaw.corey@gmail.com>
2021-09-02 16:08:28 -0500

    created multiple views

---
+++ src/main/resources/static/js/Views/partials/Navbar.js
@@ -0,0 +1,3 @@
+export default function Navbar() {
+    return `<h1>Home</h1>`
+}
--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -1,4 +1,4 @@
-
+import Home from "./Views/Home.js"
 /**
  * Returns the route object for a specific route based on the given URI
  * @param URI
@@ -6,7 +6,12 @@
  */
 export default function router(URI) {
     const routes = {
-
+         '/': {
+             returnView: Home,
+             state: {},
+             uri: '/',
+             title: 'Home',
+         }
     };
 
     return routes[URI];
