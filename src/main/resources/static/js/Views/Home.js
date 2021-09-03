15138cde9218cf3d2e1090029b55178a4e392b9d
Jesse Leffew <jesse.leffew89@gmail.com>
2021-09-03 12:10:34 -0500

    Youtube

--- src/main/resources/static/js/Views/Home.js
+++ src/main/resources/static/js/Views/Home.js
@@ -1,10 +1,11 @@
 import * as KEYS from "../keys.js";
 export default function Home() {
-    return `<input type="text" placeholder="search"> <button id="submit">Submit</button> <div id="recipe"></div> <div id="google_house"></div>`
+    return `<input id="inputMain" type="text" placeholder="search"> <button id="submit">Submit</button> <div id="recipe"></div> <div id="google_house"></div> <div id="youtubeBox"></div>`
 }
 export function searchClick(){
     $("#submit").click(function (){
         googleGet()
+        getVideos()
         })
 }
 function googleGet(){
@@ -14,4 +15,34 @@ function googleGet(){
   frameborder="0" style="border:0"
   src="https://www.google.com/maps/embed/v1/place?key=${KEYS.returnGoogleKey()}&q=San+Antonio" allowfullscreen>
 </iframe>`)
+}
+
+function getVideos(id){
+    const q = $('#inputMain').val()
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${KEYS.returnGoogleKey()}&part=snippet&q=${q}+recipes&maxResults=4&type=video&videoEmbeddable=true`;
+    const option = {
+        method: 'GET',
+        header:{
+            'Content-Type': 'application/json'
+        }
+    };
+    fetch(url, option)
+        .then(res => res.json()
+        ).then(data => {
+        $("#youtube");
+        embedData(data)
+    })
+}
+
+function embedData(data){
+    let dataArr = data.items
+    dataArr.forEach(function(video){
+        console.log(video.id)
+        $('#youtubeBox').append(`
+        <iframe class="video" col="auto" src="https://www.youtube.com/embed/${video.id.videoId}" 
+        title="YouTube video player" 
+        frameborder="=0"
+        allow="accelermeter; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>` )
+    })
 }
