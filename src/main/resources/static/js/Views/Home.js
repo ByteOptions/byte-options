691adb90280322bb32248d1d476f7990e032f72a
gradygriffin0 <grady.griffin0@gmail.com>
2021-09-07 10:54:23 -0500

    adds mapbox and snippets of US-3

--- src/main/resources/static/js/Views/Home.js
+++ src/main/resources/static/js/Views/Home.js
@@ -1,5 +1,5 @@
 import * as KEYS from "../keys.js";
-// import mapboxgl from '../../../../../../node_modules/mapbox-gl';
+// import mapboxgl from '../../node_modules/mapbox-gl';
 
 export default function Home() {
     return `<input id="inputMain" type="text" placeholder="search"><button id="submit">Submit</button> <div id="recipe">
@@ -9,7 +9,24 @@ export default function Home() {
 export function searchClick() {
     mapBox()
     $("#submit").click(function () {
+
+        // searchRecipes();
+        searchGeocoder();
     })
+}
+
+function searchGeocoder(){
+    let q = $("#inputMain").val();
+    $.ajax({
+        method: "GET",
+        url: `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/-98.4936,29.4241.json?limit=50&access_token=${mapboxgl.accessToken}&radius=3000&dedupe=false`,
+        success: function(data){
+            console.log(data);
+            console.log("tiles");
+
+        }
+    });
+
 }
 
 
@@ -18,7 +35,45 @@ function mapBox() {
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-74.5, 40], // starting position [lng, lat]
+        center: [-98.4936, 29.4241], // starting position [lng, lat]
         zoom: 9 // starting zoom
     });
+    //this adds a search bar on our map but we already use one as the main feature of the site so its commented out for now
+    // map.addControl(
+    //     new MapboxGeocoder({
+    //         accessToken: mapboxgl.accessToken,
+    //         mapboxgl: mapboxgl
+    //     })
+    // );
 }
+
+
+//first snippet of us-3 (copy/pasted)
+// // first call to spoontacular -> returns vague list of recipes with IDs
+// function searchRecipes() {
+//     let q = $("#inputMain").val();
+//     $.ajax({
+//         method: "GET",
+//         url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=${KEYS.returnSpoonKey()}&query=${q}&offset=0&number=10`,
+//         success: function (data) {
+//             console.log(data);
+//             getRecipe(data);
+//         }
+//     })
+// }
+// //second call to spoontacular, -> returns more indepth results with given ID's
+// function getRecipe(data){
+//     $.ajax({
+//         method: "GET",
+//         url: `https://api.spoonacular.com/recipes/${data.results[0].id}/information?apiKey=${KEYS.returnSpoonKey()}&includeNutrition=true`,
+//         success: function(data){
+//             console.log(data);
+//             $("#recipe").html(`${data.title}<br> <ul>${returnIngredients(data)}</ul>${data.instructions}`)
+//         }
+//     })
+//
+// }
+// // decipher getRecipe data into list items to append to html
+// function returnIngredients(data){
+//     return data.extendedIngredients.map(ingredient => `<li>${ingredient.original}</li>`).join("");
+// }
