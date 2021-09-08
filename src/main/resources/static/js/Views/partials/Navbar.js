cc4748f89430a4ea4637825e27e8f004041c30fa
shawcorey <shaw.corey@gmail.com>
2021-09-08 12:27:00 -0500

    added searchbar

--- src/main/resources/static/js/Views/partials/Navbar.js
+++ src/main/resources/static/js/Views/partials/Navbar.js
@@ -1,3 +1,25 @@
 export default function Navbar() {
-    return `<h1>Home</h1>`
+    return `<h1>ByteOptions</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"></a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Recipes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Account</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input id="search" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button id="search" class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>`
 }
