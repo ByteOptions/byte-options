b0469c3f0193bcfde806285ecaa751ae20dcf7f1
Jesse Leffew <jesse.leffew89@gmail.com>
2021-09-09 16:39:50 -0500

    register

--- src/main/resources/static/js/Views/Register.js
+++ src/main/resources/static/js/Views/Register.js
@@ -1,3 +1,4 @@
+import createView from "../createView.js";
 
 export default function Register(props){
     return `
@@ -24,7 +25,7 @@ export default function Register(props){
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input id="usernameInput" type="text" id="form3Example1c" class="form-control" />
+                      <input id="usernameInput" type="text" class="form-control" />
                       <label class="form-label" for="form3Example1c">Username</label>
                     </div>
                   </div>
@@ -32,7 +33,7 @@ export default function Register(props){
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input id="emailInput" type="email" id="form3Example3c" class="form-control" />
+                      <input id="emailInput" type="email" class="form-control" />
                       <label class="form-label" for="form3Example3c">Email</label>
                     </div>
                   </div>
@@ -40,18 +41,18 @@ export default function Register(props){
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input id="passwordInput" type="password" id="form3Example4c" class="form-control" />
+                      <input id="passwordInput" type="password" class="form-control" />
                       <label class="form-label" for="form3Example4c">Password</label>
                     </div>
                   </div>
 
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input id="passwordConfirmInput" type="password" id="form3Example4cd" class="form-control" />
-                      <label class="form-label" for="form3Example4cd">Confirm Password</label>
-                    </div>
-                  </div>
+<!--                  <div class="d-flex flex-row align-items-center mb-4">-->
+<!--                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>-->
+<!--                    <div class="form-outline flex-fill mb-0">-->
+<!--                      <input id="passwordConfirmInput" type="password" class="form-control" />-->
+<!--                      <label class="form-label" for="form3Example4cd">Confirm Password</label>-->
+<!--                    </div>-->
+<!--                  </div>-->
 
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                     <button id="registerButton" type="button" class="btn btn-primary btn-lg">Register</button>
@@ -71,6 +72,31 @@ export default function Register(props){
 </html>`;
 }
 
-export function registerEvents(){
+export function createRegisterEvents(){
+    registerEvent()
+}
+
+function registerEvent(){
+    $("#registerButton").click(function(){
+
+        let newUser = {
+            username : $("#usernameInput").val(),
+            email: $("#emailInput").val(),
+            password: $("#passwordInput").val(),
+        }
+        console.log(newUser)
+
+        let request = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newUser)
+        };
+
+        fetch("http://localhost:8080/api/users", request)
+            .then((response) => {
+                console.log(response.status)
+                createView("/")
+            });
+    })
 
 }
