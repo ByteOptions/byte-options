4eeb7ad1aa7031ca761054cb8dc872502d43c8f6
gradygriffin0 <grady.griffin0@gmail.com>
2021-09-09 12:02:36 -0500

    Restaurants view for nonuser

--- src/main/resources/static/js/Views/Restaurants.js
+++ src/main/resources/static/js/Views/Restaurants.js
@@ -1,5 +1,6 @@
 export default function Restaurants() {
-    return "<h1>OrderNow</h1>"
+    return `<h1>Register an account!</h1>
+                <p class="lead">See information catered specifically to you <br> Track recipe nutrition, favorite restaurants, fit your specific diet and more...</p>`
 };
 
 export function restaurantsEvents() {
