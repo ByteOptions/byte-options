719d281680fb7d20f3845608742e5598d6c09bef
gradygriffin0 <grady.griffin0@gmail.com>
2021-09-14 11:11:38 -0500

    fixes conflicts

--- src/main/resources/static/js/Views/Home.js
+++ src/main/resources/static/js/Views/Home.js
@@ -12,43 +12,77 @@ var geojson = {
 
 export default function Home() {
     return `  
-    <div class="container">
+  <div id="background">
+            <div id="searchHouse" class="container">
+                <br/>
+                <div class="row justify-content-center">
+                    <div id="searchBox" class="col-12 col-md-10 col-lg-8">
+                        <form class="card card-sm">
+                            <div  class="card-body row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <i class="fas fa-search h4 text-body"></i>
+                                </div>
+                                <!--end of col-->
+                                <div class="col">
+                                    <input id="inputMain" class="form-control form-control-lg form-control-borderless"
+                                           type="search" placeholder="Search topics or keywords">
+                                </div>
+                                <!--end of col-->
+                                <div class="col-auto">
+                                    <button id="search" class="searchBtn" type="button">Search</button>
+                                </div>
+                                <!--end of col-->
+                            </div>
+                        </form>
+                    </div>
+                    <!--end of col-->
+                </div>
+            </div>
+            <div id="info" class="container">
+    <div class="">
         <div class="row">
         <div class="col-md-8">
             <div id="recipe"></div>
-            <button id="prevspoon" class="d-none">Previous</button>
-            <button id="morespoon" class="d-none">More</button>
+            <button id="prevspoon" class="d-none homeBtn">Previous</button>
+            <button id="morespoon" class="d-none homeBtn">More</button>
             </div>
             <div class="col-md-4">
-                <div class="d-flex justify-content-center flex-wrap">
+                <div id="big-box" class="d-flex justify-content-center flex-wrap">
                     <div id="youtubeBox" class="d-flex justify-content-center flex-wrap"></div> 
                     <br>
-                    <button id="prevbtn" class="d-none">Previous</button> 
-                    <button id="morebtn" class="d-none" >More videos</button>
+                    <button id="prevbtn" class="d-none homeBtn">Previous</button> 
+                    <button id="morebtn" class="d-none homeBtn" >More videos</button>
                     <br>
                     <div id="google_house"></div>
-                    <div id="map" style="width: 300px; height: 250px;"></div>
+                    <div class="m-3" id="map" style="width: 300px; height: 250px;"></div>
+                    <a id="Ainfo"></a>
+
                 </div>
             </div>
         </div>
+    </div>
     </div>
          `
 }
 
 export function homeEvents() {
     searchClick();
-
+}
+function scrollToAnchor(){
+    var aTag = $("#Ainfo");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 }
 
 function searchClick() {
     $("#submit").click(function () {
         let q = $("#inputMain").val()
-        // console.log(q);
+        console.log(q);
         getLocations(q);
-        searchRecipes(q);
+        // searchRecipes();
         getVideos(q)
         mapBox()
-
+        searchRecipes(q)
+        scrollToAnchor();
     })
 }
 
@@ -69,7 +103,7 @@ function getVideos(q) {
             videoEmbeddable: true,
         },
         success: function (data) {
-            // console.log(data)
+            console.log(data)
             nextPageToken = data.nextPageToken
             // getNextVideo(q, data.nextPageToken)
             embedData(data);
@@ -115,13 +149,12 @@ function embedData(data) {
     let dataArr = data.items
     dataArr.forEach(function (video) {
         $("#youtubeBox").append(`
-            <iframe class="videoBox col-auto" src="https://www.youtube.com/embed/${video.id.videoId}" title="YouTube video player"
+            <iframe class="m-3 videoBox col-auto" src="https://www.youtube.com/embed/${video.id.videoId}" title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>`)
 
     })
-    console.log(data)
 }
 function addYoutubePagination(q){
     $("#morebtn").click(function(){
@@ -139,7 +172,8 @@ function getLocations(q) {
         method: "GET",
         url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=29.4241%2C-98.4936&radius=3000&key=${KEYS.returnGoogleKey()}&type=restaurant&keyword=${q}`,
         success: function (data) {
-            // console.log(data);
+            console.log(data);
+            console.log("locations")
             combLocation(data);
         }
     })
@@ -221,7 +255,6 @@ function combLocation(data) {
             geometry: {type: 'Point', coordinates: [lng, lat]},
             properties: {title: `${result.name}`, description: `${result.vicinity}`}
         })
-        // console.log(data)
 
 
     });
@@ -247,8 +280,8 @@ function searchRecipes(q) {
             number: 10,
         },
         success: function (data) {
-            // console.log(data);
-            ingredientsCall(data)
+            console.log(data);
+            // ingredientsCall(data)
             embedFoodAnchors(data)
             addSpoonPagination(q)
         }
@@ -266,7 +299,7 @@ function nextSpoonCall(q, offset) {
         },
         success: function (data) {
             console.log(data);
-            ingredientsCall(data)
+            // ingredientsCall(data)
             embedFoodAnchors(data)
         }
     })
@@ -277,7 +310,7 @@ function embedFoodAnchors(data){
     $("#recipe").html("");
     data.results.forEach(function(result){
         let el = $(`<a class='clickAnchor' data-id='${result.id}'>${result.title}</a>`)
-        // console.log(el);
+        console.log(el);
         $("#recipe").append(el);
         $("#recipe").append("<br>")
         el.click(function(){
@@ -326,7 +359,7 @@ function ingredientsCall(result) {
         url: `https://api.spoonacular.com/recipes/${result.id}/information?apiKey=${KEYS.returnSpoonKey()}&includeNutrition=true`,
         success: function (data) {
             console.log(data);
-            $("#recipe").html(`<button id="backbutton">Back</button> <br>${data.title}<br> <ul>${returnIngredients(data)}</ul>${data.instructions}`)
+            $("#recipe").html(`<button class="homeBtn" id="backbutton">Back</button> <br>${data.title}<br> <ul>${returnIngredients(data)}</ul>${data.instructions}`)
             $("#backbutton").click(function(){
                 nextSpoonCall(globalQ, offset)
             })
