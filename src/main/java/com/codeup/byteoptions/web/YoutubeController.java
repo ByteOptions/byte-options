8bc3e52b8d84cfb7921f4ac476f2d072a8568efa
Jesse Leffew <jesse.leffew89@gmail.com>
2021-09-14 11:24:54 -0500

    fixes merge

--- src/main/java/com/codeup/byteoptions/web/YoutubeController.java
+++ src/main/java/com/codeup/byteoptions/web/YoutubeController.java
@@ -1,4 +1,10 @@
 package com.codeup.byteoptions.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/youtubes", headers = "Accept=application/json", produces = "application/json")
 public class YoutubeController {
+
 }
