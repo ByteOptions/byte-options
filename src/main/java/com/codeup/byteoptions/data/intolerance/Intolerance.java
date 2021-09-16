e91950309907a11e290c2c99d343b8d5aa63abe8
Jesse Leffew <jesse.leffew89@gmail.com>
2021-09-16 15:15:38 -0500

    intolerance

---
+++ src/main/java/com/codeup/byteoptions/data/intolerance/Intolerance.java
@@ -0,0 +1,45 @@
+package com.codeup.byteoptions.data.intolerance;
+
+
+import net.bytebuddy.implementation.bytecode.assign.TypeCasting;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.security.PrivateKey;
+
+@Entity
+@Table(name="intolerance")
+public class Intolerance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true)
+    private String name;
+
+    public Intolerance(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Intolerance(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
