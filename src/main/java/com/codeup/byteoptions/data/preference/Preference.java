7d1d08c11123c978f6a32867da2bb8ea130c40da
Jesse Leffew <jesse.leffew89@gmail.com>
2021-09-17 12:31:55 -0500

    preference

--- src/main/java/com/codeup/byteoptions/data/preference/Preference.java
+++ src/main/java/com/codeup/byteoptions/data/preference/Preference.java
@@ -1,4 +1,28 @@
 package com.codeup.byteoptions.data.preference;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="preference")
 public class Preference {
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    public Preference(Long id) {
+        Id = id;
+    }
+
+    public Preference(){
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
 }
