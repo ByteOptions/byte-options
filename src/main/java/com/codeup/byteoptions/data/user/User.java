df0e69d8dc8b255e52149948d966dfee6a3efb71
Jesse Leffew <jesse.leffew89@gmail.com>
2021-09-20 09:47:00 -0500

    fixed user

--- src/main/java/com/codeup/byteoptions/data/user/User.java
+++ src/main/java/com/codeup/byteoptions/data/user/User.java
@@ -1,10 +1,12 @@
 package com.codeup.byteoptions.data.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.codeup.byteoptions.data.recipes.Recipe;
+import com.codeup.byteoptions.data.restaurant.Restaurant;
+import com.codeup.byteoptions.data.video.Video;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import java.util.Collection;
 
 @Entity
 @Table(name="users")
@@ -29,21 +31,63 @@ public class User {
     private Role role = Role.USER;
     public enum Role {USER, ADMIN};
 
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH},
+            targetEntity = Restaurant.class)
+    @JoinTable(
+            name="user_restaurants",
+            joinColumns = {@JoinColumn(name = "restaurant_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="user_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
+
+    private Collection<Restaurant>restaurants;
 
 
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH},
+            targetEntity = Recipe.class)
+    @JoinTable(
+            name="user_recipe",
+            joinColumns = {@JoinColumn(name = "recipe_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="user_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
 
-    public User(Long id, String username, String email, String password) {
+                private Collection<Recipe>recipes;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH},
+            targetEntity = Video.class)
+    @JoinTable(
+            name="user_videos",
+            joinColumns = {@JoinColumn(name = "video_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="user_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
+
+    private Collection<Video>videos;
+
+
+    public User(Long id, String username, String email, String password,
+                Collection<Restaurant> restaurants, Collection<Recipe> recipes,
+                Collection<Video> videos) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.restaurants = restaurants;
+        this.recipes=recipes;
+        this.videos = videos;
     }
 
     public User(){
-    }
-
-    public User (String username){
-        this.username = username;
     }
 
     public Long getId() {
@@ -78,6 +122,30 @@ public class User {
         this.password = password;
     }
 
+    public Collection<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(Collection<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public Collection<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Collection<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public Collection<Video> getVideos() {
+        return videos;
+    }
+
+    public void setVideos(Collection<Video> videos) {
+        this.videos = videos;
+    }
+
     public Role getRole() {
         return role;
     }
@@ -86,4 +154,3 @@ public class User {
         this.role = role;
     }
 }
-
